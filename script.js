8888d80f04a3f9c04988d18f35b346af00081df2
Kerstin Bonev-Wagner <kerstin-wagner@gmx.at>
2019-01-13 17:23:57 +0100

    feat: user can add new tasks

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//alert("test")
+// Add a new Task to list based on user Input
+function addTask() {
+  let userInput = document.getElementById('newTask').value
+  let li = document.createElement('LI');
+  let text = document.createTextNode(userInput);
+  li.appendChild(text);
+  if (userInput.length > 1) {
+    document.getElementById('tasks').appendChild(li);
+}
+  else {
+    alert('Please write a task')
+  }
+  document.getElementById("newTask").value = "";
+}
+
+document.getElementById('button').addEventListener('click', addTask)
