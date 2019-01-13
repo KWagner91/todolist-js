ccf3fd4071071e47e5648a35645d3953741374b9
Kerstin Bonev-Wagner <kerstin-wagner@gmx.at>
2019-01-13 17:57:21 +0100

    feat: delete symbol was added to existing and newly created Tasks

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ function addTask() {
   let li = document.createElement('LI');
   let text = document.createTextNode(userInput);
   li.appendChild(text);
+
   if (userInput.length > 1) {
     document.getElementById('tasks').appendChild(li);
+    let span = document.createElement('span');
+    let text = document.createTextNode("x");
+    span.appendChild(text);
+    document.getElementById('tasks').lastElementChild.appendChild(span);
 }
   else {
     alert('Please write a task')
@@ -14,3 +19,17 @@ function addTask() {
 }
 
 document.getElementById('button').addEventListener('click', addTask)
+
+
+
+// Add Delete Button to predefined list of Tasks
+const allLi = document.getElementsByTagName('li');
+function addDelete() {
+  [...allLi].forEach(function(element) {
+    let span = document.createElement('span');
+    let text = document.createTextNode("x");
+    span.appendChild(text);
+    element.appendChild(span);
+  })
+}
+addDelete();
